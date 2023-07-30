b6dde83659303e593b47e9ea41e6ae506969418a
Carl Osterwisch <costerwi@gmail.com>
2023-07-30 08:51:24 -0400

    Add install script

---
+++ install.cmd
@@ -0,0 +1,33 @@
+@echo off
+
+set plugin=headInjury
+
+if defined HOME (
+    set abaqus_plugins=%HOME%\abaqus_plugins
+) else (
+    set abaqus_plugins=%HOMEDRIVE%%HOMEPATH%\abaqus_plugins
+)
+
+echo This script copies the Abaqus CAE plugin "%plugin%" into "%abaqus_plugins%"
+
+if not exist "%abaqus_plugins%" (
+    echo ERROR: "%abaqus_plugins%" does not exist.
+    pause
+    exit 1
+)
+
+set destination=%abaqus_plugins%\%plugin%\
+if "%~dp0"=="%destination%" (
+    echo ERROR: Already installed.
+    pause
+    exit 1
+)
+
+if not exist "%destination%" mkdir "%destination%"
+copy /Y "%~dp0\*.*" "%destination%"
+if ERRORLEVEL 0 (
+    echo Success! Restart Abaqus CAE and check Plugin-ins menu.
+) else (
+    echo Something went wrong. Contact support@caelynx.com
+)
+pause
